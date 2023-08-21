66a2e4da98f6f3b71588cebeb6924d785d031547
Roberto Zepeda <robertojesezepedacalero@gmail.com>
2023-08-21 17:23:34 -0600

    File upload and auto Indexing support

--- app/routers/chatbot.py
+++ app/routers/chatbot.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, UploadFile
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
@@ -12,9 +12,27 @@ from app.utils import (
 
 router = APIRouter()
 
+# user uploads multiple files to a directory
 @router.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+async def create_upload_file(directory_path: str, files: list[UploadFile]):
+    if directory_path == "":
+        return JSONResponse(
+            status_code=400,
+            content= "Please specify a directory path"
+        )
+    if not os.path.exists("docs/"+directory_path):
+        os.makedirs("docs/"+directory_path)
+
+    for file in files:
+        with open("docs/"+directory_path+"/"+file.filename, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+    construct_index(directory_path)
+
+    return JSONResponse(
+        status_code=200,
+        content= "File uploaded successfully"
+    )
 
 class PrompData(BaseModel):
     question: str
--- app/utils/chatbot.py
+++ app/utils/chatbot.py
@@ -1,6 +1,3 @@
-from langchain import OpenAI
-import gradio as gr
-
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,6 +10,7 @@ from llama_index import (
     load_index_from_storage,
 )
 from llama_index.prompts import Prompt
+from llama_index.llms import OpenAI
 
 def construct_index(directory_path):
 
@@ -20,15 +18,8 @@ def construct_index(directory_path):
     parser = node_parser.SimpleNodeParser()
     nodes = parser.get_nodes_from_documents(docs)
 
-    # llm = OpenAI(model="gpt-4", temperature=70, max_tokens=1024)
-    # max_input_size = 4096
-    # num_output = 1024
-    # chunk_overlap_ratio = 0.40
-    # chunk_size_limit = 1200
-
-    # prompt_helper = PromptHelper(max_input_size, num_output, chunk_overlap_ratio, chunk_size_limit=chunk_size_limit)
-
-    service_context = ServiceContext.from_defaults( chunk_size = 1024 )
+    llm = OpenAI(model="gpt-4", temperature=0.7, max_tokens=512)
+    service_context = ServiceContext.from_defaults( llm=llm )
     index = VectorStoreIndex(nodes, service_context=service_context)
     index.storage_context.persist(persist_dir="storage/"+directory_path)
 
@@ -42,7 +33,6 @@ def chatbot_generator(input_text, prompt_text):
         replacement_prompt = Prompt(prompt_text)
         query_engine = index.as_query_engine(similarity_top_k=5, text_qa_template=replacement_prompt)
     else:
-        replacement_prompt = None
         query_engine = index.as_query_engine(similarity_top_k=5)
     response = query_engine.query(input_text)
     return {"answer": response.response, "sources": response.get_formatted_sources(length=400)}
